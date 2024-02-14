fe7f3ec2fcbd6d25e28c53f6d274135c83cbba5d
Angel Daniel <116531997+DanielCodexp@users.noreply.github.com>
2024-02-14 03:04:04 -0500

    conexion a base de datos

--- config/db.ts
+++ config/db.ts
@@ -1,9 +1,19 @@
-
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
-const sequelize = new Sequelize('contact', 'root', 'password', {
+dotenv.config(); // Cargar variables de entorno desde el archivo .env
+
+// Obtener las variables de entorno y proporcionar valores predeterminados si no están definidas
+const dbName = process.env.DB_NAME || '';
+const dbUser = process.env.DB_USER || '';
+const dbPassword = process.env.DB_PASSWORD || '';
+const dbHost = process.env.DB_HOST || 'localhost';
+const dbPort = process.env.DB_PORT || '3306';
+
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
   dialect: 'mysql',
-  host: 'localhost',
+  host: dbHost,
+  port: parseInt(dbPort, 10), // Convertir a número
 });
 
 export default sequelize;
